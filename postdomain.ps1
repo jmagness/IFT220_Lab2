c971d20c69ee6916e9b6af0d69c41f25805a39ce
jmagness <48852516+jmagness@users.noreply.github.com>
2019-04-01 00:49:44 -1000

    aaaa
    aaa

--- postdomain.ps1
+++ postdomain.ps1
@@ -2,6 +2,8 @@
 
 # Check the that the DNS server IP address on the NIC is set to our non-loopback IP and set it if needed
 $nicname = Get-NetAdapter  | select -ExpandProperty "name"
+
+
 $ipaddress = Get-NetIPAddress -InterfaceAlias $nicname -AddressFamily IPv4 | select -ExpandProperty "IPAddress"
 if ((Get-DnsClientServerAddress -InterfaceAlias $nicname -AddressFamily IPv4 | select -ExpandProperty "ServerAddresses") -ne $ipaddress) 
 {
